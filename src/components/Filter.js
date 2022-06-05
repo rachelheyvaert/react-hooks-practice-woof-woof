7e9172b18af6ef60c584f87bdbf0328dd6026ac6
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-06-05 17:00:18 -0400

    Done

---
+++ src/components/Filter.js
@@ -0,0 +1,15 @@
+
+   
+import React from "react";
+
+function Filter({ onFilterClick, goodDogsOnly }) {
+  return (
+    <div id="filter-div">
+      <button id="good-dog-filter" onClick={onFilterClick}>
+        Filter good dogs: {goodDogsOnly ? "ON" : "OFF"}
+      </button>
+    </div>
+  );
+}
+
+export default Filter;
